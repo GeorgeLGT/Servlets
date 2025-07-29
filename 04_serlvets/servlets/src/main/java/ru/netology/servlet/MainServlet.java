d5953dde89d0a0d176cdedfaf5e4df0b119e89e4
Georgy Lesyuk <rambozet@yandex.ru>
2025-07-29 22:36:42 +0300

    Implement feature/di-java #2

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,8 +1,8 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.AppConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,50 +11,46 @@ import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
 
-  private static final String PATH_POSTS = "/api/posts";
-  private static final String PATH_POSTS_ID_PREFIX = "/api/posts/";
+    private static final String PATH_POSTS = "/api/posts";
+    private static final String PATH_POSTS_ID_PREFIX = "/api/posts/";
 
+    private PostController controller;
 
-  private PostController controller;
-
-  @Override
-  public void init() {
-
-    PostRepository repository = new PostRepository();
-    PostService service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-
-      if (method.equals("GET") && path.equals(PATH_POSTS)) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.startsWith(PATH_POSTS_ID_PREFIX)) {
-        final var idStr = path.substring(PATH_POSTS_ID_PREFIX.length());
-        long id = Long.parseLong(idStr);
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals(PATH_POSTS)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.startsWith(PATH_POSTS_ID_PREFIX)) {
-        final var idStr = path.substring(PATH_POSTS_ID_PREFIX.length());
-        long id = Long.parseLong(idStr);
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        controller = context.getBean(PostController.class);
     }
-  }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            if (method.equals("GET") && path.equals(PATH_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.startsWith(PATH_POSTS_ID_PREFIX)) {
+                final var idStr = path.substring(PATH_POSTS_ID_PREFIX.length());
+                long id = Long.parseLong(idStr);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals(PATH_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.startsWith(PATH_POSTS_ID_PREFIX)) {
+                final var idStr = path.substring(PATH_POSTS_ID_PREFIX.length());
+                long id = Long.parseLong(idStr);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
