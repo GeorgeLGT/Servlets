dda922bd06e5c82f47c535baf8d48caad878aa6d
Georgy Lesyuk <rambozet@yandex.ru>
2025-07-29 23:08:29 +0300

    Implement feature/di-java 2

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,9 +1,11 @@
 package ru.netology.servlet;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.config.AppConfig;
 import ru.netology.controller.PostController;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +19,9 @@ public class MainServlet extends HttpServlet {
     private PostController controller;
 
     @Override
-    public void init() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    public void init() throws ServletException {
+        super.init();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         controller = context.getBean(PostController.class);
     }
 
